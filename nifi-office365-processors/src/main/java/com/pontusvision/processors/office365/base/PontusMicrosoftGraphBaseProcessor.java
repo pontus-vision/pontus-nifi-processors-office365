7ca02714b23be368d2eed234444c0308b437d0a9
Leo Martins <lmartins@pontusvision.com>
2021-07-05 11:01:13 +0100

    wip

---
+++ nifi-office365-processors/src/main/java/com/pontusvision/processors/office365/base/PontusMicrosoftGraphBaseProcessor.java
@@ -0,0 +1,282 @@
+package com.pontusvision.processors.office365.base;
+
+import com.pontusvision.nifi.office365.PontusMicrosoftGraphAuthControllerServiceInterface;
+import org.apache.commons.io.IOUtils;
+import org.apache.nifi.annotation.behavior.DynamicProperty;
+import org.apache.nifi.annotation.documentation.CapabilityDescription;
+import org.apache.nifi.annotation.documentation.Tags;
+import org.apache.nifi.annotation.lifecycle.OnScheduled;
+import org.apache.nifi.annotation.lifecycle.OnStopped;
+import org.apache.nifi.components.PropertyDescriptor;
+import org.apache.nifi.components.ValidationResult;
+import org.apache.nifi.components.Validator;
+import org.apache.nifi.distributed.cache.client.Deserializer;
+import org.apache.nifi.distributed.cache.client.DistributedMapCacheClient;
+import org.apache.nifi.distributed.cache.client.Serializer;
+import org.apache.nifi.expression.ExpressionLanguageScope;
+import org.apache.nifi.flowfile.FlowFile;
+import org.apache.nifi.logging.ComponentLog;
+import org.apache.nifi.processor.*;
+import org.apache.nifi.processor.exception.ProcessException;
+import org.apache.nifi.processor.util.StandardValidators;
+
+import java.nio.charset.Charset;
+import java.util.*;
+import java.util.regex.Pattern;
+
+import static com.pontusvision.nifi.office365.PontusMicrosoftGraphAuthControllerServiceInterface.getStackTrace;
+
+@Tags({ "GRAPH", "User", "Microsoft", "Office 365" }) @CapabilityDescription(
+    "Gets Office users, and adds the userID for each user"
+        + " in the office365_user_id flow file attribute")
+@DynamicProperty(name = "Generated FlowFile attribute name", value = "Generated FlowFile attribute value",
+    expressionLanguageScope = ExpressionLanguageScope.VARIABLE_REGISTRY,
+    description = "Specifies an attribute on generated FlowFiles defined by the Dynamic Property's key and value." +
+        " If Expression Language is used, evaluation will be performed only once per batch of generated FlowFiles.")
+abstract public class PontusMicrosoftGraphBaseProcessor extends AbstractProcessor
+{
+  public static final String OFFICE365_USER_ID     = "office365_user_id";
+  public static final String OFFICE365_FOLDER_ID   = "office365_folder_id";
+  public static final String OFFICE365_MESSAGE_ID  = "office365_message_id";
+  public static final String OFFICE365_DELTA_VALUE = "office365_delta";
+  public static final String OFFICE365_DELTA_KEY   = "office365_delta_key";
+  public static final String OFFICE365_CACHE_KEY   = "office365_cache_key";
+
+  public static final String OFFICE365_DELTA_KEY_FORMAT_MESSAGE_PREFIX = "O365_messages";
+  public static final String OFFICE365_DELTA_KEY_FORMAT_USER_PREFIX    = "O365_users_delta";
+  public static final String OFFICE365_DELTA_KEY_FORMAT_FOLDER_PREFIX  = "O365_folders";
+
+  public static final String OFFICE365_DELTA_KEY_FORMAT_MESSAGE = OFFICE365_DELTA_KEY_FORMAT_MESSAGE_PREFIX + "|%s|%s";
+  public static final String OFFICE365_DELTA_KEY_FORMAT_USER    = OFFICE365_DELTA_KEY_FORMAT_USER_PREFIX;
+  public static final String OFFICE365_DELTA_KEY_FORMAT_FOLDER  = OFFICE365_DELTA_KEY_FORMAT_FOLDER_PREFIX + "|%s";
+
+  public static final String OFFICE365_REGEX_MESSAGE_DEFAULT = OFFICE365_DELTA_KEY_FORMAT_MESSAGE_PREFIX + ".*";
+  public static final String OFFICE365_REGEX_USER_DEFAULT    = OFFICE365_DELTA_KEY_FORMAT_USER_PREFIX;
+  public static final String OFFICE365_REGEX_FOLDER_DEFAULT  = OFFICE365_DELTA_KEY_FORMAT_FOLDER_PREFIX + ".*";
+
+  protected List<PropertyDescriptor> properties;
+  protected Set<Relationship>        relationships;
+
+  protected DistributedMapCacheClient                          cacheClient         = null;
+  protected String                                             cacheFilterRegexStr = null;
+  protected PontusMicrosoftGraphAuthControllerServiceInterface authProviderService;
+  protected Pattern                                            cacheFilterRegex;
+
+  public static final PropertyDescriptor OFFICE365_DISTRIB_MAP_CACHE = new PropertyDescriptor
+      .Builder()
+      .name("Distributed Map Cache Client")
+      .description("A Distributed Map Cache with ids to trigger the workflow")
+      .required(true)
+      .identifiesControllerService(DistributedMapCacheClient.class)
+      .build();
+
+  public final static PropertyDescriptor CACHE_FILTER_REGEX_USER = new PropertyDescriptor.Builder()
+      .name("Cache Filter User Regex")
+      .defaultValue(OFFICE365_REGEX_USER_DEFAULT)
+      .description(
+          "Cache filter - this will choose which entries in the cache that will be processed by this processor")
+      .addValidator(StandardValidators.REGULAR_EXPRESSION_VALIDATOR)
+      .addValidator(
+          (subject, input, context) -> input.contains(OFFICE365_DELTA_KEY_FORMAT_USER_PREFIX) ?
+              Validator.VALID.validate(subject, input, context) :
+              new ValidationResult
+                  .Builder()
+                  .valid(false)
+                  .subject(subject)
+                  .input(input)
+                  .explanation("The regex must contain the string " + OFFICE365_DELTA_KEY_FORMAT_USER_PREFIX)
+                  .build())
+      .expressionLanguageSupported(ExpressionLanguageScope.FLOWFILE_ATTRIBUTES)
+      .required(true)
+      .build();
+
+  public final static PropertyDescriptor CACHE_FILTER_REGEX_MESSAGE = new PropertyDescriptor.Builder()
+      .name("Cache Filter Message Regex")
+      .defaultValue(OFFICE365_REGEX_MESSAGE_DEFAULT)
+      .description(
+          "Cache filter - this will choose which entries in the cache that will be processed by this processor")
+      .addValidator(StandardValidators.REGULAR_EXPRESSION_VALIDATOR)
+      .addValidator(
+          (subject, input, context) -> input.contains(OFFICE365_DELTA_KEY_FORMAT_MESSAGE_PREFIX) ?
+              Validator.VALID.validate(subject, input, context) :
+              new ValidationResult
+                  .Builder()
+                  .valid(false)
+                  .subject(subject)
+                  .input(input)
+                  .explanation("The regex must contain the string " + OFFICE365_DELTA_KEY_FORMAT_MESSAGE_PREFIX)
+                  .build())
+      .expressionLanguageSupported(ExpressionLanguageScope.FLOWFILE_ATTRIBUTES)
+      .required(true)
+      .build();
+
+  public final static PropertyDescriptor CACHE_FILTER_REGEX_FOLDER = new PropertyDescriptor.Builder()
+      .name("Cache Filter Folder Regex")
+      .defaultValue(OFFICE365_REGEX_FOLDER_DEFAULT)
+      .description(
+          "Cache filter - this will choose which entries in the cache that will be processed by this processor")
+      .addValidator(StandardValidators.REGULAR_EXPRESSION_VALIDATOR)
+      .addValidator(
+          (subject, input, context) -> input.contains(OFFICE365_DELTA_KEY_FORMAT_FOLDER_PREFIX) ?
+              Validator.VALID.validate(subject, input, context) :
+              new ValidationResult
+                  .Builder()
+                  .valid(false)
+                  .subject(subject)
+                  .input(input)
+                  .explanation("The regex must contain the string " + OFFICE365_DELTA_KEY_FORMAT_FOLDER_PREFIX)
+                  .build())
+      .required(true)
+      .build();
+
+  public final static PropertyDescriptor SERVICE = new PropertyDescriptor.Builder()
+      .name("Controller Service")
+      .displayName("Controller Service")
+      .description("Authentication Controller Service")
+      .required(true)
+      .identifiesControllerService(PontusMicrosoftGraphAuthControllerServiceInterface.class)
+      .build();
+
+  public static final Relationship ORIGINAL = new Relationship.Builder().name("Original")
+                                                                        .description("Success relationship").build();
+
+  public static final Relationship SUCCESS = new Relationship.Builder().name("Success")
+                                                                       .description("Success relationship").build();
+
+  public static final Relationship FAILURE = new Relationship.Builder().name("Failure")
+                                                                       .description("Failure relationship").build();
+
+  public static Serializer<String>   SER = (s, outputStream) -> outputStream.write(s.getBytes());
+  public static Deserializer<String> DES = String::new;
+
+  protected abstract PropertyDescriptor getRegexPropertyDescriptor();
+
+  @Override public void init(final ProcessorInitializationContext context)
+  {
+    List<PropertyDescriptor> properties = new ArrayList<>();
+    properties.add(SERVICE);
+    properties.add(OFFICE365_DISTRIB_MAP_CACHE);
+    properties.add(getRegexPropertyDescriptor());
+
+    this.properties = Collections.unmodifiableList(properties);
+
+    Set<Relationship> relationships = new HashSet<>();
+    relationships.add(ORIGINAL);
+    relationships.add(SUCCESS);
+    relationships.add(FAILURE);
+    this.relationships = Collections.unmodifiableSet(relationships);
+  }
+
+  public static void writeFlowFile(FlowFile flowFile, ProcessSession session, String data, Relationship relationship)
+  {
+    FlowFile ff = flowFile;
+    ff = session.write(ff, out -> IOUtils.write(data, out, Charset.defaultCharset()));
+    session.transfer(ff, relationship);
+  }
+
+  @OnScheduled
+  public void onScheduled(final ProcessContext context)
+  {
+    cacheFilterRegexStr = context.getProperty(getRegexPropertyDescriptor()).evaluateAttributeExpressions().getValue();
+    cacheFilterRegex = Pattern.compile(cacheFilterRegexStr);
+
+    if (authProviderService == null)
+    {
+      authProviderService = context.getProperty(SERVICE)
+                                   .asControllerService(
+                                       PontusMicrosoftGraphAuthControllerServiceInterface.class);
+    }
+
+    if (cacheClient == null)
+    {
+      cacheClient = context.getProperty(OFFICE365_DISTRIB_MAP_CACHE)
+                           .asControllerService(
+                               DistributedMapCacheClient.class);
+    }
+  }
+
+  @Override public void onTrigger(final ProcessContext context, final ProcessSession session) throws ProcessException
+  {
+    FlowFile flowFile = session.get();
+    if (flowFile == null)
+    {
+      flowFile = session.create();
+    }
+
+    try
+    {
+      Set<String> keys = cacheClient.keySet(DES);
+
+      long counter = 0;
+
+      for (String key : keys)
+      {
+        if (cacheFilterRegex.matcher(key).matches())
+        {
+          counter++;
+          process(context, session, flowFile, key, cacheClient.get(key, SER, DES));
+        }
+      }
+      if (counter == 0)
+      {
+        process(context, session, flowFile);
+      }
+
+      session.transfer(flowFile, ORIGINAL);
+    }
+    catch (Exception ex)
+    {
+      session.remove(flowFile);
+      handleError(getLogger(), ex, session);
+    }
+  }
+
+  @OnStopped
+  public void onStopped()
+  {
+    cacheClient = null;
+    authProviderService = null;
+  }
+
+  public void process(ProcessContext context, ProcessSession session, FlowFile flowFile, String key,
+                      String delta) throws Exception
+  {
+
+  }
+
+  public void process(ProcessContext context, ProcessSession session, FlowFile flowFile) throws Exception
+  {
+
+  }
+
+  @Override public Set<Relationship> getRelationships()
+  {
+    return relationships;
+  }
+
+  @Override public List<PropertyDescriptor> getSupportedPropertyDescriptors()
+  {
+    return properties;
+  }
+
+  public static void handleError(ComponentLog logger, Exception ex, ProcessSession session)
+  {
+    logger.error("Unable to process", ex);
+
+    FlowFile flowFile = session.create();
+    flowFile = session.putAttribute(flowFile, "Office365.Error", ex.getMessage());
+    flowFile = session.putAttribute(flowFile, "Office365.StackTrace", getStackTrace(ex));
+    session.transfer(flowFile, FAILURE);
+
+  }
+  public static void  handleError(ComponentLog logger, Exception ex, ProcessSession session, FlowFile flowFile)
+  {
+    logger.error("Unable to process", ex);
+
+    flowFile = session.putAttribute(flowFile, "Office365.Error", ex.getMessage());
+    flowFile = session.putAttribute(flowFile, "Office365.StackTrace", getStackTrace(ex));
+    session.transfer(flowFile, FAILURE);
+
+  }
+
+}
---
+++ nifi-office365-processors/src/main/java/com/pontusvision/processors/office365/base/PontusMicrosoftGraphGenericProcessor.java
@@ -0,0 +1,328 @@
+package com.pontusvision.processors.office365.base;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.microsoft.graph.http.CustomRequest;
+import com.microsoft.graph.models.extensions.IGraphServiceClient;
+import com.microsoft.graph.options.FunctionOption;
+import com.microsoft.graph.options.QueryOption;
+import com.pontusvision.nifi.office365.PontusMicrosoftGraphAuthControllerServiceInterface;
+import org.apache.commons.io.IOUtils;
+import org.apache.nifi.annotation.documentation.CapabilityDescription;
+import org.apache.nifi.annotation.documentation.Tags;
+import org.apache.nifi.components.PropertyDescriptor;
+import org.apache.nifi.components.ValidationResult;
+import org.apache.nifi.components.Validator;
+import org.apache.nifi.expression.ExpressionLanguageScope;
+import org.apache.nifi.flowfile.FlowFile;
+import org.apache.nifi.processor.*;
+import org.apache.nifi.processor.exception.ProcessException;
+import org.apache.nifi.processor.util.StandardValidators;
+import org.apache.nifi.util.StringUtils;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.*;
+
+import static com.pontusvision.processors.office365.base.PontusMicrosoftGraphBaseProcessor.writeFlowFile;
+
+@Tags({ "GRAPH", "Generic", "Microsoft", "Office 365" }) @CapabilityDescription("Get messages")
+
+public class PontusMicrosoftGraphGenericProcessor extends AbstractProcessor
+{
+
+  private List<PropertyDescriptor> properties;
+  private Set<Relationship>        relationships;
+
+  private PontusMicrosoftGraphAuthControllerServiceInterface authProviderService;
+
+  final static PropertyDescriptor URL = new PropertyDescriptor
+    .Builder()
+    .name("URL suffix")
+    .defaultValue("")
+    .description("The microsoft graph URL suffix (after the https://graph.microsoft.com/v1.0 prefix)")
+    .addValidator(StandardValidators.NON_EMPTY_VALIDATOR).required(true)
+    .expressionLanguageSupported(ExpressionLanguageScope.FLOWFILE_ATTRIBUTES)
+    .build();
+
+  final static PropertyDescriptor METHOD_TYPE = new PropertyDescriptor
+    .Builder()
+    .name("Method Type")
+    .defaultValue("GET")
+    .description("The microsoft graph API HTTP Method")
+    .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
+    .required(true)
+    .expressionLanguageSupported(ExpressionLanguageScope.FLOWFILE_ATTRIBUTES)
+    .allowableValues("GET","PUT","POST","DELETE","PATCH")
+    .build();
+
+  final static PropertyDescriptor DATA = new PropertyDescriptor
+    .Builder()
+    .name("DATA")
+    .defaultValue("")
+    .description("The data (if required) to be sent to the API; ignored if not present")
+    .addValidator(new StandardValidators.StringLengthValidator(0,1000000000))
+    .required(false)
+    .expressionLanguageSupported(ExpressionLanguageScope.FLOWFILE_ATTRIBUTES)
+    .build();
+
+  final static PropertyDescriptor SEARCH_FIELDS = new PropertyDescriptor
+    .Builder()
+    .name("Search Fields").defaultValue(
+        "id,createdDateTime,lastModifiedDateTime,changeKey,categories,receivedDateTime,sentDateTime,hasAttachments,internetMessageId,subject,bodyPreview,importance,parentFolderId,conversationId,isDeliveryReceiptRequested,isReadReceiptRequested,isRead,isDraft,webLink,inferenceClassification,body,sender,from,toRecipients,ccRecipients,bccRecipients,replyTo,flag")
+    .description("Optional Fields to return from the Office 365 Graph API Calls."
+        + "If left blank, this will return all fields.  Examples: subject,body.content,sender,from,"
+        + "toRecipients,ccRecipients")
+    .addValidator(new StandardValidators.StringLengthValidator(0,1000000000))
+    .required(true)
+    .expressionLanguageSupported(ExpressionLanguageScope.FLOWFILE_ATTRIBUTES)
+    .build();
+
+  final static Validator jsonValidator =  (subject, input, context) -> {
+    ValidationResult.Builder builder = new ValidationResult.Builder();
+    builder.subject(subject).input(input);
+    if (context.isExpressionLanguageSupported(subject) && context.isExpressionLanguagePresent(input)) {
+      return builder.valid(true).explanation("Contains Expression Language").build();
+    } else {
+      try {
+        if (!StringUtils.isBlank(input)) {
+          JsonParser parser = new JsonParser();
+          parser.parse(input);
+        }
+        builder.valid(true);
+      } catch (Exception var6) {
+        builder.valid(false).explanation(var6.getMessage());
+      }
+      return builder.build();
+    }
+  };
+
+  final static PropertyDescriptor FUNCTION_OPTIONS = new PropertyDescriptor
+    .Builder()
+    .name("Function Options")
+    .defaultValue("")
+    .description("Optional Function Options; ignored if not present")
+    .addValidator(jsonValidator)
+    .required(false)
+    .expressionLanguageSupported(ExpressionLanguageScope.FLOWFILE_ATTRIBUTES)
+    .build();
+
+  final static PropertyDescriptor QUERY_OPTIONS = new PropertyDescriptor
+    .Builder()
+    .name("Query Options")
+    .defaultValue("")
+    .description("Optional Query Options; ignored if not present")
+    .addValidator(jsonValidator)
+    .required(false)
+    .expressionLanguageSupported(ExpressionLanguageScope.FLOWFILE_ATTRIBUTES)
+    .build();
+
+  final static PropertyDescriptor HEADERS = new PropertyDescriptor
+    .Builder()
+    .name("Headers")
+    .defaultValue("")
+    .description("Optional Headers; ignored if not present")
+    .addValidator(jsonValidator)
+    .required(false)
+    .expressionLanguageSupported(ExpressionLanguageScope.FLOWFILE_ATTRIBUTES)
+    .build();
+
+
+  final static PropertyDescriptor SERVICE = new PropertyDescriptor
+    .Builder()
+    .name("Auththentication Controller Service")
+    .displayName("Authentication Controller Service")
+    .description("Controller Service to authenticate with Office 365 using oauth2")
+    .required(true)
+    .identifiesControllerService(PontusMicrosoftGraphAuthControllerServiceInterface.class)
+    .build();
+
+
+  public static final Relationship SUCCESS = PontusMicrosoftGraphBaseProcessor.SUCCESS;
+
+
+  public static final Relationship FAILURE = PontusMicrosoftGraphBaseProcessor.FAILURE;
+
+
+
+  @Override public void init(final ProcessorInitializationContext context)
+  {
+    List<PropertyDescriptor> properties = new ArrayList<>();
+    properties.add(SERVICE);
+    properties.add(URL);
+    properties.add(METHOD_TYPE);
+    properties.add(DATA);
+    properties.add(SEARCH_FIELDS);
+    properties.add(FUNCTION_OPTIONS);
+    properties.add(QUERY_OPTIONS);
+    properties.add(HEADERS);
+
+    this.properties = Collections.unmodifiableList(properties);
+
+    Set<Relationship> relationships = new HashSet<>();
+    relationships.add(FAILURE);
+    relationships.add(SUCCESS);
+    this.relationships = Collections.unmodifiableSet(relationships);
+  }
+
+
+  public void processData (FlowFile flowFile, ProcessContext context, ProcessSession session, String data){
+    JsonParser parser = new JsonParser();
+
+    final String url = context.getProperty(URL).evaluateAttributeExpressions(flowFile).getValue();
+    final String methodType = context.getProperty(METHOD_TYPE).evaluateAttributeExpressions(flowFile).getValue();
+//    session.remove(flowFile);
+    final IGraphServiceClient graphClient = authProviderService.getService();
+
+    final CustomRequest<JsonObject> req = graphClient.customRequest(url).buildRequest();
+
+    final String selectStr = context.getProperty(SEARCH_FIELDS).evaluateAttributeExpressions(flowFile).getValue();
+
+    if (StringUtils.isNotBlank(selectStr)){
+      req.select(selectStr);
+    }
+
+    final String functionOptionsStr = context
+        .getProperty(FUNCTION_OPTIONS)
+        .evaluateAttributeExpressions(flowFile)
+        .getValue();
+
+    if (StringUtils.isNotEmpty(functionOptionsStr)) {
+      JsonObject funcOptions = parser.parse(functionOptionsStr).getAsJsonObject();
+
+      funcOptions.entrySet()
+        .forEach(stringJsonElementEntry -> req.addFunctionOption(new FunctionOption(stringJsonElementEntry.getKey(),
+          stringJsonElementEntry.getValue().getAsString()))
+        );
+    }
+    final String queryOptionsStr = context
+        .getProperty(QUERY_OPTIONS)
+        .evaluateAttributeExpressions(flowFile)
+        .getValue();
+    if (StringUtils.isNotEmpty(queryOptionsStr)) {
+
+      JsonObject queryOptions = parser.parse(queryOptionsStr).getAsJsonObject();
+
+      queryOptions.entrySet()
+        .forEach(stringJsonElementEntry -> req.addQueryOption(new QueryOption(stringJsonElementEntry.getKey(),
+          stringJsonElementEntry.getValue().getAsString()))
+        );
+    }
+
+    final String headersStr = context
+        .getProperty(HEADERS)
+        .evaluateAttributeExpressions(flowFile)
+        .getValue();
+
+    if (StringUtils.isNotEmpty(headersStr)) {
+
+      JsonObject headers = parser.parse(headersStr).getAsJsonObject();
+
+      headers.entrySet()
+        .forEach(stringJsonElementEntry -> req.addHeader(stringJsonElementEntry.getKey(),
+          stringJsonElementEntry.getValue().getAsString())
+        );
+    }
+    JsonObject obj = null;
+    if("GET".equalsIgnoreCase(methodType)){
+      obj = req.get();
+    }
+    else if("POST".equalsIgnoreCase(methodType)){
+      JsonObject postObj = parser.parse(data).getAsJsonObject();
+      obj = req.post(postObj);
+    }
+    else if("PUT".equalsIgnoreCase(methodType)){
+      JsonObject postObj = parser.parse(data).getAsJsonObject();
+      obj = req.put(postObj);
+    }
+    else if("PATCH".equalsIgnoreCase(methodType)){
+      JsonObject postObj = parser.parse(data).getAsJsonObject();
+      obj = req.patch(postObj);
+    }
+    else if("DELETE".equalsIgnoreCase(methodType)){
+      req.delete();
+    }
+
+    if (obj != null){
+      writeFlowFile(flowFile,session,obj.toString(),SUCCESS );
+    }
+
+
+    //      session.transfer(flowFile, ORIGINAL);
+  }
+
+  @Override public void onTrigger(final ProcessContext context, final ProcessSession session) throws ProcessException
+  {
+    FlowFile flowFile = session.get();
+
+    if (flowFile == null)
+    {
+      flowFile =  session.create();
+    }
+    String data = null;
+    try
+    {
+      data = context.getProperty(DATA).evaluateAttributeExpressions(flowFile).getValue();
+
+      if (StringUtils.isBlank(data))
+      {
+        data = readFromFlowFile(session,flowFile);
+  //      if (StringUtils.isBlank(data)) {
+  //        getLogger().error("Unable to process flow File; must add the attribute data");
+  //        session.transfer(flowFile, FAILURE);
+  //        return;
+  //      }
+      }
+
+      if (authProviderService == null)
+      {
+        authProviderService = context
+          .getProperty(SERVICE)
+          .asControllerService(PontusMicrosoftGraphAuthControllerServiceInterface.class);
+      }
+
+
+      processData(flowFile,context,session,data);
+    }
+    catch (Exception e)
+    {
+      try
+      {
+        authProviderService.refreshToken();
+        processData(flowFile,context,session,data);
+      }
+      catch (Exception ex2)
+      {
+        PontusMicrosoftGraphBaseProcessor.handleError(getLogger(), ex2, session, flowFile );
+      }
+    }
+
+  }
+
+  private String readFromFlowFile(ProcessSession session, FlowFile flowFile) {
+    final StringBuilder sb = new StringBuilder();
+    session.read(flowFile, in -> {
+      try
+      {
+        String queryStr = IOUtils.toString(in, Charset.defaultCharset());
+        sb.append(queryStr);
+      }
+      catch (Exception e)
+      {
+        getLogger().error("Failed to run query against Tinkerpop server; error: {}", e);
+        throw new IOException(e);
+      }
+    });
+    return sb.toString();
+  }
+
+  @Override public Set<Relationship> getRelationships()
+  {
+    return relationships;
+  }
+
+  @Override public List<PropertyDescriptor> getSupportedPropertyDescriptors()
+  {
+    return properties;
+  }
+}
